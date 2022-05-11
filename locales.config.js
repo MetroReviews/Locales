1175b11a1a467230279b17a12b5a57979fa8dd0e
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2022-05-10 23:13:54 -0600

    Add files via upload

---
+++ locales.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    en: {
+        iso: "en",
+        name: "English",
+        country: "gb",
+    },
+    fr: {
+        iso: "fr",
+        name: "Français",
+        country: "fr",
+    },
+};
